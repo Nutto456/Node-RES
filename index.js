0d54eeb2acb9892a940bb075aff059800ca5e529
Nutto456 <s6506022610022@email.kmutnb.ac.th>
2023-09-05 09:06:59 +0700

    1

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! Hello! Hello!!!! Hello!!@");
+  res.send("Hello World! Hello! Hello!!!! Hello!!");
 });
 
 app.listen(port, () => {
