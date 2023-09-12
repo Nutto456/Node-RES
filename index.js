b51e5e74f20c72b20b96cc4ad47d5b1309b5af43
Nutto456 <s6506022610022@email.kmutnb.ac.th>
2023-09-12 11:09:03 +0700

    update

--- index.js
+++ index.js
@@ -25,8 +25,8 @@ app.get('/books', (req, res) => {
     });
 });
 
-app.get('/books', (req, res) => {
-    db.all('SELECT * FROM books WHERE id = ?', req.params.id, (err, row) => {
+app.get('/books/:id', (req, res) => {
+    db.get('SELECT * FROM books WHERE id = ?', req.params.id, (err, row) => {
         if (err) {
             res.status(500).send(err);
         } else {
@@ -62,7 +62,7 @@ app.put('/books/:id', (req, res) => {
     });
 });
 
-app.delete('/book/:id', (req, res) => {
+app.delete('/books/:id', (req, res) => {
     db.run('DELETE FROM BOOKS WHERE id = ?', req.params.id, function(err) {
         if (err) {
             res.status(500).send(err);
